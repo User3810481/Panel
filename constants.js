d426b0f89ba8196b978006136e087e065716ef63
User3810481 <hya377497@gmail.com>
2025-07-05 21:57:25 +0800

    Update constants.js

--- constants.js
+++ constants.js
@@ -3,6 +3,6 @@ export const DEFAULT_API_HOST = 'localhost';
 export const DEFAULT_API_PORT = '19222';
 
 export function setApiUrl(host, port) {
-  API_URL = `https://${host}:${port}/`;
+  API_URL = `http://${host}:${port}/`;
   console.log('API URL set to:', API_URL); // For debugging
 }
